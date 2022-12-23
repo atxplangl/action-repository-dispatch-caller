a40cea8d58cf0967e94ca316eb9a78409bfc8129
Phillip Langley <philliplangley@champmbplangley.lan>
2022-12-23 14:40:53 -0600

    adding action for dispatch caller

--- action-repository-dispatch-caller.py
+++ action-repository-dispatch-caller.py
@@ -14,12 +14,13 @@ def main:
     event = os.environ{"EVENT"}
     github_ref = os.environ{"GITHUB_REF"}
     github_sha = os.environ{"GITHUB_SHA"}
-url = "https://api.github.com/repos/" + org_name "/" + repo_name "/dispatches"
 
-payload = {"event_type": event, "client_payload": {"ref": github_ref, "sha": github_sha}}
+    url = "https://api.github.com/repos/" + org_name "/" + repo_name "/dispatches"
 
-header = {"Accept": "application/vnd.github+json", "Authorization": "token " github_token}
-payload = json.dumps(payload)
-response = requests.post(url=url, headers=header, json=payload)
+    payload = {"event_type": event, "client_payload": {"ref": github_ref, "sha": github_sha}}
 
+    header = {"Accept": "application/vnd.github+json", "Authorization": "token " github_token}
+    payload = json.dumps(payload)
+    response = requests.post(url=url, headers=header, json=payload)
+main()
 
